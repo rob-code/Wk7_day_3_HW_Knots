2b45f08add54691dc954b186ac3569fa4d92df62
Robert Brice <rob.a.brice@gmail.comC>
2017-03-15 21:09:14 +0000

    updated images and their reference id's

--- app/src/main/java/com/codeclan/topknot/KnotList.java
+++ app/src/main/java/com/codeclan/topknot/KnotList.java
@@ -9,20 +9,19 @@ private ArrayList<Knot> list;
     public KnotList(){
 
         list = new ArrayList<Knot>();
-        list.add(new Knot("Reef Knot", "Tying the reefs of sails", R.drawable.bowline));
-        list.add(new Knot("Half Hitch", "A stopper knot - but best not to use as it locks", R.drawable.bowline));
-        list.add(new Knot("Sheet Bend", "Join 2 ropes", R.drawable.bowline));
-        list.add(new Knot("Figure of 8 Knot", "A stopper at the end of a rope", R.drawable.bowline));
-        list.add(new Knot("Slip Knot", "Let something go quickly", R.drawable.bowline));
-        list.add(new Knot("Noose", "Make a loop in a rope", R.drawable.bowline));
+        list.add(new Knot("Reef Knot", "Tying the reefs of sails", R.drawable.reef));
+        list.add(new Knot("Sheep Shank", "A kind of a block and tackle", R.drawable.sheepshank));
+        list.add(new Knot("Sheet Bend", "Joins 2 ropes", R.drawable.sheetbend));
+        list.add(new Knot("Figure of 8 Knot", "A stopper at the end of a rope", R.drawable.fig8_));
+        list.add(new Knot("Slip Knot", "Let something go quickly!", R.drawable.slip));
+        list.add(new Knot("Noose", "Make a loop in a rope", R.drawable.noose));
         list.add(new Knot("Bowline", "Make a better loop in the end of a rope", R.drawable.bowline));
-        list.add(new Knot("Bowline on a Bight", "Make a loop in the middle of a rope", R.drawable.bowline));
-        list.add(new Knot("Running Bowline", "Makes a loop through which an end can run", R.drawable.bowline));
-        list.add(new Knot("Round Turn and Two Half Hitches", "Quick know for securing a line to a post", R.drawable.bowline));
-        list.add(new Knot("Carrick Bend", "Strong way of attached two ropes of equal size", R.drawable.bowline));
-        list.add(new Knot("Single Sheet Bend", "A secure way of attached a thin rope to a thick loop", R.drawable.bowline));
-        list.add(new Knot("Double Sheet Bend", "A better way of attaching a thin rope to a think loop", R.drawable.bowline));
-
+        list.add(new Knot("Round Turn and Two Half Hitches", "Quick know for securing a line to a post", R.drawable.roundturn));
+        list.add(new Knot("Carrick Bend", "Strong way of attached two ropes of equal size", R.drawable.carrick));
+        list.add(new Knot("Rolling Hitch", "Take the load of a loaded rope", R.drawable.rollinghitch));
+        list.add(new Knot("Clove Hitch", "Secure a line perpendicular to another", R.drawable.cloveend));
+        list.add(new Knot("Double Fisherman's bend", "A fast and secure way to join two ropes", R.drawable.doublefishermans));
+        list.add(new Knot("Figure of 8 bend", "Another fast and secure way to join two ropes", R.drawable.fig8join));
     }
 
     public ArrayList<Knot> getList(){
