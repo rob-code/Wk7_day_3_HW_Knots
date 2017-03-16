eafb7884c71039d0f477355238858674727b5378
Robert Brice <rob.a.brice@gmail.comC>
2017-03-16 12:48:58 +0000

    add a new activity on clicking a list item and passing info to this activity

---
+++ app/src/main/java/com/codeclan/topknot/InfoActivity.java
@@ -0,0 +1,21 @@
+package com.codeclan.topknot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+
+    TextView knotText;
+    KnotList list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    Intent intent = getIntent();
+    Bundle extras = intent.getExtras();
+}
